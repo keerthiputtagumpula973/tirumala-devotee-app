f3cffd86802ee67312df3ec4cb6ead853a951f3c
keerthiputtagumpula973 <keerthiputtagumpula973@gmail.com>
2026-06-30 11:05:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['tirumala-devotee-app.onrender.com']
+  },
   preview: {
     allowedHosts: ['tirumala-devotee-app.onrender.com']
   }
